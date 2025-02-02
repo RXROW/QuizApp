db58aeed8c9badd8f1601043dcecae9b88fc94a0
“Eslam“ <aslamalmswdya@gmail.com>
2025-02-02 17:03:36 +0200

    app refresh fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,35 @@
-import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';  
-import { QuizProvider } from './context/QuizContext';
-import { QuizApp } from './components/quiz/QuizApp';
-import Signup from './components/auth/Signup';
-import Login from './components/auth/Login';
-import Navbar from './components/layout/Navbar';
-import Footer from './components/layout/Footer';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import UserProvider, { useAuth } from './context/UserContext';
-import Profile from './components/profile/Profile';
-import Dashboard from './components/dashboard/Dashboard';
-import CreateQuiz from './components/dashboard/CreateQuiz/CreateNewQuiz';
-import CreateNewSubject from './components/dashboard/CreateSubject/CreateNewSubject';
-import Users from './components/dashboard/users/Users';
-import ForgetPassword from './components/auth/ForgetPassword';
-import ProtectedRoute from './ProtectedRoute'; // Import the ProtectedRoute component
-
+import React from "react";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { QuizProvider } from "./context/QuizContext";
+import { QuizApp } from "./components/quiz/QuizApp";
+import Signup from "./components/auth/Signup";
+import Login from "./components/auth/Login";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import UserProvider, { useAuth } from "./context/UserContext";
+import Profile from "./components/profile/Profile";
+import Dashboard from "./components/dashboard/Dashboard";
+import CreateQuiz from "./components/dashboard/CreateQuiz/CreateNewQuiz";
+import CreateNewSubject from "./components/dashboard/CreateSubject/CreateNewSubject";
+import Users from "./components/dashboard/users/Users";
+import ForgetPassword from "./components/auth/ForgetPassword";
+import ProtectedRoute from "./ProtectedRoute";
+import { useNavigate } from "react-router-dom";
 function App() {
+  const { user } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (user) {
+      navigate("/");
+    } else {
+      navigate("/login");
+    }
+  }, [user, navigate]);
   return (
-    <Router>  
+    <Router>
       <UserProvider>
         <QuizProvider>
           <ToastContainer
@@ -41,11 +51,26 @@ function App() {
             <Route path="/forget-password" element={<ForgetPassword />} />
 
             {/* Protected Routes */}
-            <Route path="/profile" element={<ProtectedRoute element={<Profile />} />} />
-            <Route path="/dashboard" element={<ProtectedRoute element={<Dashboard />} />} />
-            <Route path="/dashboard/create-quiz" element={<ProtectedRoute element={<CreateQuiz />} />} />
-            <Route path="/dashboard/create-subject" element={<ProtectedRoute element={<CreateNewSubject />} />} />
-            <Route path="/dashboard/users" element={<ProtectedRoute element={<Users />} />} />
+            <Route
+              path="/profile"
+              element={<ProtectedRoute element={<Profile />} />}
+            />
+            <Route
+              path="/dashboard"
+              element={<ProtectedRoute element={<Dashboard />} />}
+            />
+            <Route
+              path="/dashboard/create-quiz"
+              element={<ProtectedRoute element={<CreateQuiz />} />}
+            />
+            <Route
+              path="/dashboard/create-subject"
+              element={<ProtectedRoute element={<CreateNewSubject />} />}
+            />
+            <Route
+              path="/dashboard/users"
+              element={<ProtectedRoute element={<Users />} />}
+            />
           </Routes>
           <Footer />
         </QuizProvider>
